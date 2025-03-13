fb099b288875f5dc0fb023646f607d33f9d88c26
Helena <ukil89@mail.ru>
2025-03-14 02:05:15 +0300

    add js

---
+++ script.js
@@ -0,0 +1,63 @@
+let authData = {};
+
+function sendData() {
+  const inputs = document.querySelectorAll(".reset__form_input");
+  inputs.forEach((input) => {
+    authData[input.name] = input.value;
+  });
+
+  const username = document.getElementById("username");
+  const secret = document.getElementById("secret");
+  const password = document.getElementById("password");
+  const passwordTwo = document.getElementById("password_two");
+
+  const arr = [
+    {
+      elem: username,
+      hasError: username.value === "",
+    },
+    {
+      elem: secret,
+      hasError: secret.value === "",
+    },
+    {
+      elem: password,
+      hasError: password.value.length < 6,
+    },
+    {
+      elem: passwordTwo,
+      hasError: password.value != passwordTwo.value,
+    },
+  ];
+
+  let hasErrors = false;
+
+  for (let elem of arr) {
+    if (elem.hasError) {
+      elem.elem.parentElement.querySelector(".reset__form_warning").style.visibility = "visible";
+      hasErrors = true;
+    } else {
+      elem.elem.parentElement.querySelector(".reset__form_warning").style.visibility = "hidden";
+    }
+  }
+
+  if (hasErrors) {
+    return;
+  }
+
+  console.log(authData);
+}
+
+function togglePassword(id) {
+  const passwordField = document.getElementById(id);
+  const currentType = passwordField.type;
+  const isShow = document.getElementById("toggle-password");
+
+  if (currentType === "password") {
+    passwordField.type = "text";
+    isShow.textContent = "скрыть";
+  } else {
+    passwordField.type = "password";
+    isShow.textContent = "показать";
+  }
+}
